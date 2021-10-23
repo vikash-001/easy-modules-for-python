519707ca8e2667a7c0da7daf709dc5c170914f76
Vikashkumar0007 <72148380+Vikashkumar0007@users.noreply.github.com>
2021-10-23 09:45:43 +0530

    Create shutdown_pc.py

---
+++ shutdown_pc.py
@@ -0,0 +1,8 @@
+#pip install os
+
+import os
+shutdown = input("Do you want to shutdown your computer (yes / no): ")
+if shutdown == 'yes':
+    os.system("shutdown /s /t 1")
+else:
+    print('Shutdown is not requested')                                               
